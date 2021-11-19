faf70f9ac66aa49d07b10e76c5e9c13561874df3
pc161 <pc161@DESKTOP-P22KOIM>
2021-11-19 14:38:45 +0200

    Добавьте файлы проекта.

---
+++ CS1/Program.cs
@@ -0,0 +1,10 @@
+﻿int x;
+int y;
+
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+
+int a = Math.Abs (x) - Math.Abs(y);
+int b = 1 + Math.Abs(y * x);
+int equ = a / b;
+Console.WriteLine ("equ: " + equ);
